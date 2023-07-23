1a9a42e8037373fec3d08d97472759b286e5294b
Vince Black <127775448+vinceblackdark@users.noreply.github.com>
2023-07-23 21:20:29 +0200

    Point.cs
    create an object 2*1 string.

---
+++ Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+using System.Drawing;
+using static System.Drawing.Color;
+namespace MayaEngine
+{
+    public class Point
+    {
+        private int _posY { get; set; }
+        private int _posX { get; set; }
+        private Color _color { get; set; }
+        private string _pointChar { get; set; }
+
+        public Point(int posX, int posY, Color color, char pointChar)
+        {
+            _color = color;
+            _pointChar = pointChar.ToString();
+            _posY = posY;
+            _posX = posX;
+        }
+        public Point(int posX, int posY, Color color)
+        {
+            _color = color;
+            _pointChar = "░░";
+            _posY = posY;
+            _posX = posX;
+        }
+
+        public void Draw()
+        {
+            Render.WriteStringPastel(_posX , _posY, _pointChar, _color);
+        }
+        public void DrawBg()
+        {
+            Render.WriteStringPastel(_posX, _posY, _pointChar, _color);
+        }
+    }
+}
